61a5f054377e5f24856af8c95bce748a2993e8b3
igogra <ivan.gomez.grande@gmail.com>
2016-12-11 14:31:53 +0100

    Add Module1

---
+++ Module1/app.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+        };
+    }
+})();
