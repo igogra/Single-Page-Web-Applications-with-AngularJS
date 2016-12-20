b8c5eae3331c773a53e134b708df61883f2cb2cf
Iván Gómez <ivangomez@mmedios.local>
2016-12-20 13:21:36 +0100

    Add one more item

--- Module2/app.js
+++ Module2/app.js
@@ -32,7 +32,8 @@
             { name: "cookies", quantity: 10 },
             { name: "cokes", quantity: 2 },
             { name: "beers", quantity: 6 },
-            { name: "apples", quantity: 4 }
+            { name: "apples", quantity: 4 },
+            { name: "bananas", quantity: 7 }
         ];
         var alreadyBoughtItems = [];
 
