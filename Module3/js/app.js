f6b353222d2cbf772a03d6ee42540cd2764c639b
Iván Gómez <ivangomez@mmedios.local>
2017-01-26 13:19:32 +0100

    Add secure protocol

--- Module3/js/app.js
+++ Module3/js/app.js
@@ -4,7 +4,7 @@
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
         .directive('foundItems', FoundItems);
 
     function FoundItems() {
